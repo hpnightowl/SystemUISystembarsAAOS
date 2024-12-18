d6e816aa6aa32be9d7fd7c0160a93fffa66d7360
hpnightowl <iamhp2k@gmail.com>
2024-12-18 20:40:00 +0530

    add build package in make

--- rro.mk
+++ rro.mk
@@ -1,3 +1,17 @@
-# System Bars RRO
+# No System Bars RRO
 PRODUCT_PACKAGES += \
-    CarNoSystemUIBars
+    CarNoSystemUIBarsRRO
+	
+#  Enable Bottom Bar
+PRODUCT_PACKAGES += \
+    CarSystemUIBottomRRO
+	
+#  Enable Left Bar
+PRODUCT_PACKAGES += \
+    CarSystemUILeftRRO
+	
+#  Enable Right Bar
+PRODUCT_PACKAGES += \
+    CarSystemUIRightRRO
+	
+
